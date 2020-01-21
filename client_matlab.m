ba2e6b5edb859cf3b1659ce02cfbf405fe04ebe5
diego hinojosa <dhinojosa@openworldlatam.com>
2020-01-21 14:19:47 -0300

    Add check and parse click message

--- client_matlab.m
+++ client_matlab.m
@@ -1,6 +1,19 @@
-t = tcpip('0.0.0.0', 8765)
-fopen(t)
+t = tcpip('0.0.0.0', 8765);
+fopen(t);
 
 while 1
-    data = fscanf(t)
+    data = fscanf(t);
+    if data ~= "";  %check empty data
+        datastr = strip(data) %strip data;
+        if strcmp(datastr,'DONE');
+           break %close socket
+        end
+        if datastr[0] =='#';
+           fprintf('good click\n');
+           % check click and parse
+        end
+    end
 end
+
+ fclose(t)
+ fprintf('Client closed!\n') 
