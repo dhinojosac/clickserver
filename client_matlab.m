c03dd90efb019edc7012a230b18215dbaf851125
Diego Hinojosa <d.hinojosa.cordova@gmail.com>
2020-01-21 22:14:12 -0300

    Parse point in script and read in simulink

--- client_matlab.m
+++ client_matlab.m
@@ -1,16 +1,25 @@
 t = tcpip('0.0.0.0', 8765); %socket server configuration
 fopen(t);   %open client
 
+px = -1;
+px = -1;
+save('px');
+save('py');
+
 while 1  %eternal loop to receive mouse clicks
     data = fscanf(t);
     if data ~= "";  %check empty data
-        datastr = strip(data) %strip data;
+        datastr = strip(data); %strip data;
         if strcmp(datastr,'DONE');
            break %close socket
         end
         if datastr[0] =='#';
-           fprintf('good click\n');
            % check click and parse
+           nstr = strrep(datastr,"#","");
+           nstr = strrep(nstr,"$","");
+           astr = split(nstr,",");           
+           px = str2double(astr(1));
+           py = str2double(astr(2));
         end
     end
 end
